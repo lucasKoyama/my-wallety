37b689905380d1b763e744a5fec9b1f1ed104c2c
Lucas Koyama <lucaskoyamahhh@gmail.com>
2023-06-13 14:56:38 -0300

    melhoria na tela de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,45 @@
 import React from 'react';
-import { PropTypes } from 'prop-types';
-import { TextField, Button } from '@mui/material';
-import './Login.css';
+import PropTypes from 'prop-types';
+import { TextField, Button, Box, createTheme, ThemeProvider } from '@mui/material';
+import validator from 'validator';
+import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
+import LockRoundedIcon from '@mui/icons-material/LockRounded';
+import LockOpenIcon from '@mui/icons-material/LockOpen';
 import { connect } from 'react-redux';
 import { userLoginData, fetchCurrencies } from '../redux/actions';
+import './Login.css';
+
+const theme = createTheme();
 
 class Login extends React.Component {
   state = {
     email: '',
     password: '',
+    error: false,
+    capsLockOn: false,
   };
 
-  handleChange = ({ target }) => this.setState({ [target.name]: target.value });
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+    this.validationInput();
+  };
+
+  validationInput = () => {
+    const { email } = this.state;
+    this.setState({ error: !(validator.isEmail(email) || email.length === 0) });
+  };
+
+  // Detect Caps Lock
+  detectCapsPress = (event) => {
+    const capsLockState = event.getModifierState('CapsLock');
+    this.setState({ capsLockOn: capsLockState });
+  };
+
+  detectCapsClick = (event) => {
+    const { capsLockOn } = this.state;
+    const capsLockState = event.getModifierState('CapsLock');
+    this.setState({ capsLockOn: (capsLockOn || capsLockState) && capsLockState });
+  };
 
   handleLogin = () => {
     const { dispatch, history } = this.props;
@@ -22,37 +50,63 @@ class Login extends React.Component {
   };
 
   render() {
-    const { email, password } = this.state;
-    const emailValidation = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    const passValid = 6;
+    const { email, password, loading, error, capsLockOn } = this.state;
     return (
       <section className="login-section">
         <div className="user-login">
           <p className="title">Faça seu login</p>
-          <TextField
-            label="Email"
-            type="text"
-            inputProps={ { 'data-testid': 'email-input' } }
-            name="email"
-            id="user-email"
-            value={ email }
-            onChange={ this.handleChange }
-            style={ { width: '100%', marginBottom: '10px' } }
-          />
-          <TextField
-            label="Senha"
-            type="password"
-            inputProps={ { 'data-testid': 'password-input' } }
-            name="password"
-            id="user-password"
-            value={ password }
-            onChange={ this.handleChange }
-            style={ { width: '100%', marginBottom: '10px' } }
-          />
+          <Box sx={ { position: 'relative' } }>
+            <ThemeProvider theme={ theme }>
+              <PersonRoundedIcon />
+              <TextField
+                error={ error }
+                helperText={ error && 'Email inválido' }
+                label="Email"
+                type="text"
+                name="email"
+                id="user-email"
+                value={ email }
+                onChange={ this.handleChange }
+                style={ { width: '100%', marginBottom: '15px' } }
+                focused={ email !== '' }
+                sx={ {
+                  '& .MuiInputLabel-root': {
+                    transform: email !== ''
+                      ? 'translate(14px, -9px) scale(0.75)' : 'translate(40px, 16px)',
+                    transition: 'transform 200ms cubic-bezier(0.0, 0, 0.2, 1) 0ms',
+                  },
+                } }
+              />
+            </ThemeProvider>
+          </Box>
+          <Box sx={ { position: 'relative' } }>
+            { password !== '' ? <LockOpenIcon /> : <LockRoundedIcon />}
+            <TextField
+              helperText={ capsLockOn && 'Caps lock ON!' }
+              label="Senha"
+              type="password"
+              name="password"
+              id="user-password"
+              value={ password }
+              onChange={ this.handleChange }
+              onKeyUp={ this.detectCapsPress }
+              onClick={ this.detectCapsClick }
+              style={ { width: '100%', marginBottom: '15px' } }
+              focused={ password !== '' }
+              sx={ {
+                '& .MuiInputLabel-root': {
+                  transform: password !== ''
+                    ? 'translate(14px, -9px) scale(0.75)' : 'translate(40px, 16px)',
+                  transition: 'transform 200ms cubic-bezier(0.0, 0, 0.2, 1) 0ms',
+                },
+              } }
+            />
+          </Box>
           <Button
-            disabled={ !(emailValidation.test(email) && password.length >= passValid) }
-            onClick={ () => this.handleLogin() }
+            disabled={ !(validator.isEmail(email)) }
             className="login-btn"
+            loading={ loading }
+            onClick={ () => this.handleLogin() }
           >
             Entrar
           </Button>
